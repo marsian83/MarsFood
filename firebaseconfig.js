10818992a38f83f936ba6fa262e9d9d035509c6e
marsian8 <spanarve@gmail.com>
2022-10-12 03:53:37 +0530

    apikey safe

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,17 +1,24 @@
-const {initializeApp} = require("firebase/app")
+const { initializeApp } = require("firebase/app");
 
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZH6U2g6LukmL_CMKUgmOuQkFkn1xV6Cw",
-  authDomain: "marsfood-image-storage.firebaseapp.com",
-  projectId: "marsfood-image-storage",
-  storageBucket: "marsfood-image-storage.appspot.com",
-  messagingSenderId: "147397188469",
-  appId: "1:147397188469:web:bea477f70ff5cce5a3113c",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 module.exports = { app };
