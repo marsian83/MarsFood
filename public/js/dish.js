f1581cd03599670cb062eec53b72eacc6e26e5d5
marsian8 <spanarve@gmail.com>
2022-10-12 12:13:43 +0530

    cleaned up unencesaryfiles

--- public/js/dish.js
+++ public/js/dish.js
@@ -21,10 +21,12 @@ async function getAverageRGB(src) {
     let img = new Image();
     img.src = src;
     img.crossOrigin = "";
+    img.onerror = console.error
     img.onload = () => {
       context.drawImage(img, 0, 0, 1, 1);
       resolve(context.getImageData(0, 0, 1, 1).data.slice(0, 3));
     };
+
   });
 }
 
