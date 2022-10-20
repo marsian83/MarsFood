d9fbf467c2fcf143b6cdc17a732343f32ead635e
marsian8 <spanarve@gmail.com>
2022-10-20 15:52:23 +0530

    changes

--- public/js/search.js
+++ public/js/search.js
@@ -7,20 +7,43 @@ async function fetchDishes(query) {
 async function renderResults() {
   let searchInput = document.getElementById("searchbar").value;
   let resultsHolder = document.getElementById("results-holder");
-  resultsHolder.innerHTML = "";
   if (searchInput) {
+    resultsHolder.innerHTML = `<div class="sk-circle">
+    <div class="sk-circle1 sk-child"></div>
+    <div class="sk-circle2 sk-child"></div>
+    <div class="sk-circle3 sk-child"></div>
+    <div class="sk-circle4 sk-child"></div>
+    <div class="sk-circle5 sk-child"></div>
+    <div class="sk-circle6 sk-child"></div>
+    <div class="sk-circle7 sk-child"></div>
+    <div class="sk-circle8 sk-child"></div>
+    <div class="sk-circle9 sk-child"></div>
+    <div class="sk-circle10 sk-child"></div>
+    <div class="sk-circle11 sk-child"></div>
+    <div class="sk-circle12 sk-child"></div>
+  </div>`;
     let results = await fetchDishes(searchInput);
+    resultsHolder.innerHTML=""
     results.forEach((result) => {
-      resultsHolder.innerHTML += `<div class="container card">${result.name}</div>`;
+      resultsHolder.innerHTML += `<div class="container card" onclick="location.href='/user/dish/${result.dish_id}'">
+      <img src="${result.image_url}" alt="dish-thumbnail" loading="lazy"></img> 
+      <h5>${result.name}</h5>
+      </div>`;
     });
+  }
+  else{
+    resultsHolder.innerHTML=""
   }
 }
 
-var timer = setTimeout(() => {
-  renderResults();
-}, 800);
+var prevState=""
 
-document.getElementById("searchbar").addEventListener("input", () => {
-  window.clearTimeout(timer);
-  timer = setTimeout(renderResults(), 800);
-});
+var searchbar  = document.getElementById('searchbar');
+var timer  = setTimeout(function(){renderResults()}, 0);
+
+searchbar.addEventListener('keypress', function(){
+  clearTimeout(timer); 
+  timer = setTimeout(function() 
+    { renderResults() }, 800 
+)}
+)
